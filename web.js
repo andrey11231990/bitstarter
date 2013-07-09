72132ba3c395c0cf8071efc7b32d1e9b0525292e
Andrey <andrey11231990@gmail.com>
2013-07-09 19:18:06 +0000

    another draft for web.js

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var buff = new Buffer(30);
-buff.write(fs.readFileSync(index.html));
-  response.send(buff.toString('utf-8'));
+//var buff = new Buffer(30);
+//buff.write(fs.readFileSync('index.html'));
+//  response.send(buff.toString('utf-8'));
+    response.send(fs.readFileSync('index.html').toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
